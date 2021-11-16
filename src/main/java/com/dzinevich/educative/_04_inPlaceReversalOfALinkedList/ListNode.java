c68e18c8127ec94d21ef2063a8ad525a345235a3
dzinevich <darjakisel@gmail.com>
2021-11-16 16:10:03 +0200

    ReverseLinkedList.

---
+++ src/main/java/com/dzinevich/educative/_04_inPlaceReversalOfALinkedList/ListNode.java
@@ -0,0 +1,10 @@
+package com.dzinevich.educative._04_inPlaceReversalOfALinkedList;
+
+public class ListNode {
+    int value;
+    ListNode next;
+
+    ListNode(int value) {
+        this.value = value;
+    }
+}
