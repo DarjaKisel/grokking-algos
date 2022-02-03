40ba946b8157be93bba619844b4bfbd33cc31b30
dzinevich <darjakisel@gmail.com>
2022-02-03 22:21:45 +0200

    leetcode 1446. Amend ConsecutiveCharacters. given a string of consecutive repeated characters and max value, return another string of max repeated characters.

--- src/main/java/com/dzinevich/leetcode/strings/ConsecutiveCharacters.java
+++ src/main/java/com/dzinevich/leetcode/strings/ConsecutiveCharacters.java
@@ -29,10 +29,40 @@ public class ConsecutiveCharacters {
          return max;
     }
 
+    // given a string of consecutive repeated characters and max value,
+    // return another string of max repeated characters
+    // for ex: aaaaabbbbbcccdd and max is 2 should be aabbccdd
+    public String maxConsecutiveString(String s, int k) {
+        int l = 0;
+        int r = l +1;
+
+        StringBuilder res = new StringBuilder();
+
+        while (r < s.length()) {
+            int times = 1;
+            while (r < s.length() && s.charAt(l) == s.charAt(r)) {
+
+                times++;
+                l = r;
+                r++;
+
+                if (times == k) {
+                    res.append(String.valueOf(s.charAt(l)).repeat(k));
+                }
+            }
+            l = r;
+            r++;
+        }
+        return res.toString();
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println(new ConsecutiveCharacters().maxPower("leetcode") + " : ex 2");
         System.out.println(new ConsecutiveCharacters().maxPower("abbcccddddeeeeedcba") + " : ex 5");
         System.out.println(new ConsecutiveCharacters().maxPower("ccbccbb") + " : ex 2");
+        System.out.println(new ConsecutiveCharacters().maxConsecutiveString("aaaaabbbbbcccdd", 2) + " : ex aabbccdd");
     }
 
 }
