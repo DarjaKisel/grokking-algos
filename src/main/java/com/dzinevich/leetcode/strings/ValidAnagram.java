953c152613fb78eb69a101c2231dc63af8ea3c14
dzinevich <darjakisel@gmail.com>
2022-02-06 20:39:22 +0200

    leetcode-242. ValidAnagram

--- src/main/java/com/dzinevich/leetcode/strings/ValidAnagram.java
+++ src/main/java/com/dzinevich/leetcode/strings/ValidAnagram.java
@@ -1,6 +1,8 @@
 package com.dzinevich.leetcode.strings;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ValidAnagram {
 
@@ -27,5 +29,24 @@ public class ValidAnagram {
     public static void main(String[] args) {
         System.out.println(new ValidAnagram().isAnagram("anagram", "nagaram") + ": should be true");
         System.out.println(new ValidAnagram().isAnagram("rat", "cat") + ": should be false");
+
+        System.out.println(new ValidAnagram().isValidAnagram("anagram", "nagaram") + ": should be true");
+        System.out.println(new ValidAnagram().isValidAnagram("rat", "cat") + ": should be false");
+    }
+
+    private boolean isValidAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> countS = new HashMap<>();
+        Map<Character, Integer> countT = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            countS.put(s.charAt(i), 1+ countS.getOrDefault(s.charAt(i), 0));
+            countT.put(t.charAt(i), 1+ countT.getOrDefault(t.charAt(i), 0));
+        }
+
+        return countS.equals(countT);
     }
 }
